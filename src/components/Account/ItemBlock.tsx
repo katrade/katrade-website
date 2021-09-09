c8d5dd0c056a6980ad37679dda1493f81615c228
FranKydeSU <franapasin.s@gmail.com>
2021-09-10 04:17:25 +0700

    franky did

--- src/components/Account/ItemBlock.tsx
+++ src/components/Account/ItemBlock.tsx
@@ -7,30 +7,30 @@ import { RiDeleteBin2Fill } from 'react-icons/ri';
 
 export default function ItemBlock(props: any) {
 
-    const { data, index, manage } = props;
+	const { data, index, manage } = props;
 
-    const dataTag = data.tag.map((dataTag: any) =>
-        <p className="tag">{dataTag}</p>
-    );
+	const dataTag = data.tag.map((dataTag: any) =>
+		<p className="tag">{dataTag}</p>
+	);
 
-    return (
-        <AccountBlock padding="10px">
-            <div className="block-item">
-                <div className="rowling">
-                    <div className="column-img">
-                        <img src={data.image} />
-                    </div>
-                    <div className="column-data">
-                        <h5>{data.name}</h5>
-                        <p>require : </p>
-                        {dataTag}
-                    </div>
-                    <div className={manage.length == 3 ? "column-tik" : "column-tik-hide"}>
-                        <h6 className="pen"><FaPen /></h6>
-                        <h6 className="bin"><RiDeleteBin2Fill /></h6>
-                    </div>
-                </div>
-            </div>
-        </AccountBlock>
-    );
+	return (
+		<AccountBlock padding="10px">
+			<div className="block-item">
+				<div className="rowling">
+					<div className="column-img">
+						<img src={data.image} />
+					</div>
+					<div className="column-data">
+						<h5>{data.name}</h5>
+						<p>require : </p>
+						{dataTag}
+					</div>
+					<div className={manage.length == 3 ? "column-tik" : "column-tik-hide"}>
+						<h6 className="pen"><FaPen /></h6>
+						<h6 className="bin"><RiDeleteBin2Fill /></h6>
+					</div>
+				</div>
+			</div>
+		</AccountBlock>
+	);
 }
