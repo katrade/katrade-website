db6de4ba4735039440045e9835a57370bcaa40d8
FranKydeSU <franapasin.s@gmail.com>
2021-09-19 04:37:23 +0700

    pointer cursor in LMenuAccount.

--- src/pages/ProfileViewer.tsx
+++ src/pages/ProfileViewer.tsx
@@ -62,35 +62,34 @@ export default function ProfileViewer() {
             <Block height="auto" backgroundColor="#f7fafc">
                 {/* ส่วนของข้อมูลผู้ใช้ */}
                 <div className="container-profile">
-                    <div className="container-profile-data p-3">
-                        <div className="row ">
-                            <div className="col-lg  text-center justify-content-center align-self-center">
-                                <img 
-                                    src="https://data.whicdn.com/images/349884984/original.jpg" 
-                                    style={{width:"250px",borderRadius:"50%"}}
-                                />
+                    <div className="container-profile-data">
+                        <div className="full-width d-flex justify-content-center align-items-center p-5 flex-wrap">
+                            <div className="d-flex justify-content-center mt-2 mb-3">
+                                <div style={{
+                                    backgroundImage: `url(https://asset.vg247.com/genshin-impact-yoimiya-build.jpg/BROK/thumbnail/1200x1200/quality/100/genshin-impact-yoimiya-build.jpg)`, backgroundPosition: 'center',
+                                    backgroundSize: 'cover',
+                                    backgroundRepeat: 'no-repeat',
+                                    minWidth: '130px',
+                                    minHeight: '130px',
+                                    borderRadius: '50%',
+                                    margin: '0px 50px',
+                                }}></div>
                             </div>
-                            <div className="col-lg ">
-                                <div className="row ">
-                                    <span>Username</span><h4>FranKydeSU</h4>
-                                </div>
-                                <div className="row "><span>
-                                    Firstname</span><h4>Napasin</h4>
-                                </div>
-                                <div className="row ">
-                                    <span>Following</span><h4>9999</h4>
-                                </div>
+                            <div>
+                                <h3 style={{ color: "#4a5659" }}>Napasin Saengthong</h3>
+                                <p style={{ color: "#86979c" }}>@frankydesu</p>
                             </div>
-                            <div className="col-lg  align-self-end">
-                                <div className="row">
-                                    <span>Surname</span><h4>Saengmunjaa</h4>
-                                </div>
-                                <div className="row ">
-                                    <span>Followers</span><h4>9999</h4>
-                                </div>
+                        </div>
+                        <div className="full-width d-flex justify-content-center align-items-center px-5 mb-3 flex-wrap">
+                            <div className="d-flex justify-content-center align-items-center followers mx-2 px-5 py-3 my-2">
+                                <b className="mx-2 number">9999 </b> followers
+                            </div>
+                            <div className="d-flex justify-content-center align-items-center following mx-2 px-5 py-3 my-2">
+                                <b className="mx-2 number">9999</b> following
                             </div>
                         </div>
                     </div>
+
                 </div>
                 {/* Inventory ของผู้ใช้ */}
                 <div className="my-4">
@@ -100,7 +99,7 @@ export default function ProfileViewer() {
                         <div>
                             <div className="d-flex justify-content-around flex-wrap">
                                 {inventory_item.length <= 10 ? inventory_item : inventory_item.slice(0, 10)}
-                            </div>    
+                            </div>
                         </div>
                     </div>
                 </div>
