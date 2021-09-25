f50ff855dda46da793e5754c12f4d9c90002a9d7
nutchanonc <nutchanonc@github.com>
2021-09-25 23:50:09 +0700

    remove config

--- src/firebase/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDvL2R8yDZZ_hDLkGY2bwTzHzlAd50n-0k",
-    authDomain: "letskatrade.firebaseapp.com",
-    projectId: "letskatrade",
-    storageBucket: "letskatrade.appspot.com",
-    messagingSenderId: "38538431222",
-    appId: "1:38538431222:web:d6335af8344eca7a523ad7",
-    measurementId: "G-JYTM2XHQ65"
-};
