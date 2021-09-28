df3dfbb2f9c34afa813f22c5fe97ebe2716954bc
root <root@LAPTOP-79NR55N1.localdomain>
2021-09-28 23:29:22 +0700

    1% of Change Password

--- src/components/AboutAccountComp/ChangePassComp.tsx
+++ src/components/AboutAccountComp/ChangePassComp.tsx
@@ -1,17 +1,43 @@
+import { useState } from 'react';
+import { useForm } from '../../utils/useForm';
+import { useHistory } from 'react-router-dom';
+import useLoading from '../../hooks/useLoading';
+
 export default function AccountComp(data: any) {
     const accountData = data.data;
+    const [show, hide] = useLoading();
+    const [showPassword, setShowPassword] = useState(1);
+
     return (
         <div>
             <div className="bg-white row mx-auto mb-4" style={{ width: "100%" }}>
                 <div className="p-3">
                     <h4 className="mb-4">Change Password</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, aperiam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, aperiam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, aperiam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, aperiam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, aperiam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, aperiam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, aperiam.</p>
+                        <div className="row mb-3" style={{width:"100%"}}>
+                            <label className="col-lg-3 col-form">Current Password</label>
+                            <input 
+                                className="input-none form-control border border-secondary px-2 col-lg-6"  type={showPassword === 1 ? "password" : "text"} 
+                                placeholder="current password"
+                            />
+                            <label className="blue-font-link col-auto text-end px-4" >Forgot Password?</label>
+                        </div>
+                        <div className="row mb-3" style={{width:"100%"}}>
+                            <label className="col-lg-3 col-form">New Password</label>
+                            <input 
+                                className="input-none form-control border border-secondary px-2 col-lg-6"  type={showPassword === 1 ? "password" : "text"} 
+                                placeholder="new password"
+                            />
+                        </div>
+                        <div className="row mb-5" style={{width:"100%"}}>
+                            <label className="col-lg-3 col-form">Confirm New Password</label>
+                            <input 
+                                className="input-none form-control border border-secondary px-2 col-lg-6"  type={showPassword === 1 ? "password" : "text"} 
+                                placeholder="confirm new password"
+                            />
+                        </div>
+                        <div className="row mb-3" style={{width:"100%"}}>
+                            <button type="button" className="btn btn-success col-md-2 offset-md-3">Save Changes</button>
+                        </div>
                 </div>
             </div>
         </div>
