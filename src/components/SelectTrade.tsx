a6c1e9e608985c7cec82b38d583cc52ed409445f
Nutchanon Chantrasup <nutchanonc>
2021-10-01 23:23:22 +0700

    better UI

--- src/components/SelectTrade.tsx
+++ src/components/SelectTrade.tsx
@@ -1,49 +1,67 @@
 import { ContactsOutlined } from '@material-ui/icons';
 import useAuthorization from '../hooks/useAuthorization';
+import { CgShoppingBag } from "react-icons/all"
+import "./SelectTrade.css";
+export default function SelectTrade({ onClose, array, detailItem }: any) {
 
-export default function SelectTrade({onClose,array,detailItem} : any) {
-
-    const { postMyReqeust } = useAuthorization();    
-    function handleRequest(myIdItem:any){
+    const { postMyReqeust } = useAuthorization();
+    function handleRequest(myIdItem: any) {
         const dataArray = {
-            userId2: detailItem.owner ,
-            inventoryId1: myIdItem ,
-            inventoryId2: detailItem._id ,
+            userId2: detailItem.owner,
+            inventoryId1: myIdItem,
+            inventoryId2: detailItem._id,
         }
         // console.log(dataArray);
         postMyReqeust(dataArray);
     }
 
-    const listMyInventory = array.map((data:any,index:any) => {
+    const listMyInventory = array.map((data: any, index: any) => {
+        console.log(data);
         return (
-            <div className="p-2">
-                <div onClick={() => {
-                    handleRequest(data._id);
-                }} style={{border:"1px solid rgb(112, 112, 112)",borderRadius:"6px"}}>
-                    <p className="m-0 px-3">{data.name}</p>
-                </div>
-            </div>
+            <div onClick={() => {
+                handleRequest(data._id);
+            }}
+                style={{
+                    width: "100%",
+                    height: "50px",
+                    backgroundColor: "#fff",
+                    borderRadius: "8px",
+                    margin: "7px 0",
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    border: "1px solid #d5d7db",
+                    cursor: "pointer",
+                }}
+                className="d-flex justify-content-start align-items-center"
+            >
+                <p className="m-0 px-3 me-1" style={{ color: "#000", fontWeight: 500 }}>{data.name}</p>
+                <p className="m-0 px-3 me-1" style={{ color: "#999ba1", fontWeight: 400, fontSize: "16px" }}>{data.detail}</p>
+            </div >
         );
     })
 
 
     return (
-        <div className="position-fixed" style={{zIndex:99,top:"0",bottom:"0",right:"0",left:"0"}}>
-            <div className="position-absolute" onClick={onClose} style={{backgroundColor:"rgba(0,0,0,0.1)",top:"0",bottom:"0",right:"0",left:"0"}} />
-            <div className="position-relative bg-white rounded-3"  style={{width:"500px",height:"500px",margin:"auto",marginTop:"100px"}}>
-                <p className="m-0 pt-2 text-center fs-3">Choose your item</p>
-                <hr/>
-                <div className="row">
-                    <div className="col">
-                        <p className="m-0 text-center">Match!!</p>
+        <div className="position-fixed" style={{ zIndex: 99, top: "0", bottom: "0", right: "0", left: "0", }}>
+            <div className="position-absolute" onClick={onClose} style={{ backgroundColor: "rgba(0,0,0,0.5)", top: "0", bottom: "0", right: "0", left: "0" }} />
+            <div className="position-relative bg-white rounded-3 py-1" style={{ width: "500px", height: "auto", margin: "auto", marginTop: "100px", boxShadow: "0 0 20px rgba(10,10,10,0.3)" }}>
+                <div className="m-0 p-0 rounded-top full-width d-flex justify-content-center align-items-center" style={{ backgroundColor: "#fff", height: "70px" }}>
+                    <p className="m-0" style={{ fontWeight: 500, color: "#000" }}><CgShoppingBag className="me-2" />Select your item</p>
+                </div>
+                <div className="row m-0 mx-3" style={{ width: "auto", backgroundColor: "transparent" }}>
+                    <div className="col select-items-menu p-0">
+                        <p className="m-0 text-center">Matched</p>
                     </div>
-                    <div className="col">
-                        <p className="m-0 text-center">All of your inventory</p>
+                    <div className="col select-items-menu p-0">
+                        <p className="m-0 text-center">All</p>
                     </div>
                 </div>
-                <hr/>
-                {listMyInventory}
+                <div className="px-3 py-1" style={{ backgroundColor: '#f0f2f5', minHeight: "500px", maxHeight: "700px" }}>
+                    {listMyInventory}
+                </div>
+
             </div>
-        </div>      
+        </div>
     );
 }
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom'
 
 import { useHistory } from "react-router";
@@ -13,6 +13,7 @@ import SelectTrade from '../components/SelectTrade';
 import { BsStarFill } from "react-icons/bs";
 import { SolidButton, TransparentButton } from '../components/standard/Button';
 import { AiOutlineConsoleSql } from 'react-icons/ai';
+import { FcLike, CgArrowsExchangeAlt } from 'react-icons/all';
 
 const queryString = require("query-string");
 
@@ -20,17 +21,17 @@ function Product() {
     const { search } = useLocation();
     const { product_id } = queryString.parse(search);
 
-    const { getMyInventory , 
-        getDetailProduct , 
-        getUserData , 
-        postMyReqeust , 
-        addFavourite , 
-        deleteFavourite , 
-        getAnotherUser } = useAuthorization();    
-    const [ data , setData] = useState<any>(null);
-    const [ owner , setOwner ] = useState<any>(null);
-    const [ inventory , setInventory ] = useState<any>();
-    const [ mobile , setMobile ] = useState(false);
+    const { getMyInventory,
+        getDetailProduct,
+        getUserData,
+        postMyReqeust,
+        addFavourite,
+        deleteFavourite,
+        getAnotherUser } = useAuthorization();
+    const [data, setData] = useState<any>(null);
+    const [owner, setOwner] = useState<any>(null);
+    const [inventory, setInventory] = useState<any>();
+    const [mobile, setMobile] = useState(false);
 
     const history = useHistory();
 
@@ -42,7 +43,7 @@ function Product() {
             if (dataDetail) {
                 setData(dataDetail);
             }
-            var getUser:any = await getUserData();
+            var getUser: any = await getUserData();
             if (getUser) {
                 setOwner(getUser);
             }
@@ -55,12 +56,12 @@ function Product() {
         console.log(data)
     }, [])
 
-    const [ checkFavoritetmp , setCheckFavoritetmp ] = useState<boolean>();
+    const [checkFavoritetmp, setCheckFavoritetmp] = useState<boolean>();
     useEffect(() => {
-        if(owner != null && data != null){
+        if (owner != null && data != null) {
             setCheckFavoritetmp(owner.favourite.includes(data._id))
         }
-    } , [data,owner])
+    }, [data, owner])
 
     var forOwner = 0;
     if (data && owner) {
@@ -70,17 +71,17 @@ function Product() {
     }
 
     window.addEventListener("resize", resize);
-    function resize(){
-        if(window.innerWidth < 600){
+    function resize() {
+        if (window.innerWidth < 600) {
             setMobile(true)
-        }else{
+        } else {
             setMobile(false)
         }
     }
 
-    const [ selectPhoto , setSelectPhoto ] = useState<any>(null);
-    const [ posiitonPhoto , setPositionPhoto ] = useState<any>(null);
-    function clickPhoto(position:any) {
+    const [selectPhoto, setSelectPhoto] = useState<any>(null);
+    const [posiitonPhoto, setPositionPhoto] = useState<any>(null);
+    function clickPhoto(position: any) {
         setSelectPhoto("click");
         setPositionPhoto(position);
     }
@@ -93,11 +94,11 @@ function Product() {
         photoPost = <ProductPost onBgClick={closePhoto} photoLink={data.pictures[posiitonPhoto]} />
     }
 
-    const [ selectTrade , setSelectTrade ] = useState<any>(null);
+    const [selectTrade, setSelectTrade] = useState<any>(null);
     function clickRequest() {
-        if(owner.inventories.length == 0){
+        if (owner.inventories.length == 0) {
             window.alert("คุณยังไม่มีสิ่งของเลย ไปเพิ่มก่อนสิ")
-        }else{
+        } else {
             setSelectTrade("Click")
         }
     }
@@ -110,51 +111,51 @@ function Product() {
     }
 
 
-    const [ requireDetail , SetRequireDetail ] = useState<any>();
-    const [ tmpRequireDetailShow , setTmpRequireDetailShow ] = useState<any>("m-0");
-    function changeRequireDetail(index:any) {
+    const [requireDetail, SetRequireDetail] = useState<any>();
+    const [tmpRequireDetailShow, setTmpRequireDetailShow] = useState<any>("m-0");
+    function changeRequireDetail(index: any) {
         SetRequireDetail(data.require[index].detail);
         setTmpRequireDetailShow("d-none")
     }
 
-    function handleRequest(){
+    function handleRequest() {
         const dataArray = {
-            userId2: data.owner ,
-            inventoryId1: data._id ,
-            inventoryId2: "owner._iditem" ,
+            userId2: data.owner,
+            inventoryId1: data._id,
+            inventoryId2: "owner._iditem",
         }
         postMyReqeust(dataArray);
     }
-
+    
     // const [ favorite , setFavorite] = useState(true);
     const handleClickFavorite = () => setCheckFavoritetmp(!checkFavoritetmp);
     function favorite_btn() {
-        if(!checkFavoritetmp){
+        if (!checkFavoritetmp) {
             return (
-                <SolidButton onClick={() => addFavourite(data._id)} width="132px" fontSize="24px" buttonColor="red" padding="5px" margin="0">
-                    Add to Favorite
+                <SolidButton onClick={() => addFavourite(data._id)} className="px-3 d-flex justify-content-center align-items-center" width="50px" height="50px" fontSize="24px" buttonColor="transparent" margin="0" style={{boxShadow: "0 0 8px rgba(10,10,10,0.1)", color: "#ed2b3e", border: "1px solid #ed2b3e"}}>
+                    <FcLike />
                 </SolidButton>
             );
-        }else{
+        } else {
             return (
-                <SolidButton onClick={() => deleteFavourite(data._id , checkpath)} width="132px" fontSize="24px" buttonColor="orange" padding="5px" margin="0">
-                    Remove from Favorite
+                <SolidButton onClick={() => deleteFavourite(data._id, checkpath)} className="px-3 d-flex justify-content-center align-items-center" width="50px" height="50px" fontSize="24px" buttonColor="transparent" margin="0" style={{boxShadow: "0 0 8px rgba(10,10,10,0.1)", backgroundColor: "#ed2b3e", border: "1px solid #ed2b3e", color: "#fff"}}>
+                    <FcLike style={{ filter: "brightness(10)"}}/>
                 </SolidButton>
             );
         }
     }
 
-    if(data && owner){
+    if (data && owner) {
         // console.log(data._id , owner.favourite)
         // console.log(owner.favourite.includes(data._id))
-        var checkFavorite:any = owner.favourite.includes(data._id);
+        var checkFavorite: any = owner.favourite.includes(data._id);
         var checkpath = window.location.pathname;
 
-        const wantCate = data.require.map((data:any , index:any) => {
+        const wantCate = data.require.map((data: any, index: any) => {
             return (
-                <div key={index} 
+                <div key={index}
                     onClick={() => changeRequireDetail(index)}
-                    className="bagde bagde-sm rounded-pill px-2 m-1 py-1" 
+                    className="bagde bagde-sm rounded-pill px-2 m-1 py-1"
                     style={{ backgroundColor: "#EDF2F4" }}>
                     <p className="m-0">{data.reqCat.parentCategoryEn} : {data.reqCat.childCategoryEn}</p>
                 </div>
@@ -168,29 +169,29 @@ function Product() {
             <div>
                 {photoPost}
                 {requestTrade}
-                <Navbar />
+                <Navbar image={owner.profilePic}/>
                 <Block height="auto" backgroundColor="#f7fafc">
                     {/* <div className="py-3 px-5 my-3 bg-white"> */}
                     <div className={mobile ? "py-2 px-2 my-3 bg-white" : "py-3 px-5 my-3 bg-white"}>
 
 
-                        <div className="row mx-auto" style={{width:"100%"}}>
+                        <div className="row mx-auto" style={{ width: "100%" }}>
                             <div className="col-lg py-3">
-                                <div className="mb-3 d-flex justify-content-center" style={{ height: "auto" }}>
-                                    <div style={{ width: "350px", height: "auto", backgroundColor: "#F1F1F1", padding: "30px 10px" }}>
-                                        <img className="my-auto" src={data.pictures[0]} style={{ width: "100%", height: "170px", cursor: "zoom-in" }} onClick={() => clickPhoto(0)} />
+                                <div className="mb-3 full-width" style={{ height: "auto" }}>
+                                    <div style={{ aspectRatio: "6/4", height: "auto", backgroundColor: "#F1F1F1", padding: "0" }}>
+                                        <img className="my-auto" src={data.pictures[0]} style={{ width: "100%", height: "100%", cursor: "zoom-in" }} onClick={() => clickPhoto(0)} />
                                     </div>
                                 </div>
-                                <div className="d-flex align-items-center justify-content-around" style={{ width: "auto", height: "120px", backgroundColor: "gray" }}>
-                                    { data.pictures.map((data:any , index:any) => (
+                                <div className="d-flex align-items-center justify-content-around" style={{ width: "auto", height: "120px", backgroundColor: "#F1F1F1" }}>
+                                    {data.pictures.map((data: any, index: any) => (
                                         <div key={index}>
                                             <img className="my-auto" src={data} style={{ width: "100px", height: "100px", cursor: "zoom-in" }} onClick={() => clickPhoto(index)} />
                                         </div>
                                     ))}
                                 </div>
                             </div>
-    
-                            <div className="col-lg" style={{width:"100%"}}>
+
+                            <div className="col-lg" style={{ width: "100%" }}>
                                 <h4 className="m-0">{data.name}</h4>
                                 <div className="d-flex align-items-center">
                                     <BsStarFill />
@@ -198,47 +199,52 @@ function Product() {
                                     <p className="m-0">Favorites</p>
                                 </div>
                                 <div className="d-flex mt-3 border border-secondary rounded-3">
-                                    <p className="m-0 rounded-left px-4 fw-bold p-1" style={{ color: "white", backgroundColor: "#F66464" }}>Category</p>
-                                    <div className="d-inline-block bagde bagde-sm rounded-pill px-2 m-1 py-1" style={{ backgroundColor: "#EDF2F4" }}><p className="m-0">{data.category.parentCategoryEn} : {data.category.childCategoryEn}</p></div>
+                                    <p className="m-0 rounded-left px-4 fw-bold p-1 d-flex justify-content-center align-items-center" style={{ color: "white", backgroundColor: "#F66464" }}>Category</p>
+                                    <div className="d-inline-block bagde bagde-sm rounded-pill px-2 m-1 py-1" style={{ backgroundColor: "#1c64eb", }}><p className="m-0 text-white px-3">{data.category.parentCategoryEn} : {data.category.childCategoryEn}</p></div>
                                 </div>
-                                <div className="mt-2 px-3 py-1 border border-secondary rounded-3" style={{height:"150px", overflow:"auto"}}>
-                                    <p className="mb-1 fw-bold" style={{ color: "black" }}>Requirement Detail</p>
+                                <div className="mt-2 px-3 py-1 border border-secondary rounded-3" style={{ height: "150px", overflow: "auto" }}>
+                                    <p className="mb-1 fw-bold" style={{ color: "black" }}>Detail</p>
                                     <p className="m-0">{data.detail}</p>
                                 </div>
-                                <div className="d-flex align-s-center justify-content-around mt-3" style={{ backgroundColor: "#F1F1F1" }}>
-                                    <img className="rounded-circle" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" style={{ width: "70px", height: "70px" }} />
-                                    <div onClick={() => history.push(`/app/profileviewer?user_id=${data.owner}`)} style={{cursor:"pointer"}}>
-                                        <p className="m-0 fs-3 fw-bold" style={{ color: "black" }}>{data.username}</p>
-                                        <p className="m-0" style={{ color: "black" }}>2 Follow</p>
+                                <div className="d-flex align-items-center justify-content-around mt-3" style={{ backgroundColor: "#F1F1F170", padding: "10px 0", borderRadius: "7px", boxShadow: "0 0 8px rgba(10,10,10,0.1)"}}>
+                                    <div className="d-flex">
+                                        <img className="rounded-circle me-3" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" style={{ width: "40x", height: "40px" }} />
+                                        <div className="d-flex align-items-center" onClick={() => history.push(`/app/profileviewer?user_id=${data.owner}`)} style={{ cursor: "pointer" }}>
+                                            <p className="m-0 p-0">
+                                                <b className="me-3" style={{ color: "#000", fontSize: "25px", fontWeight: 500 }}>{data.username}</b>
+                                                <span style={{ color: "#9e9e9e", fontSize: "18px" }}>N/A Followers</span>
+                                            </p>
+                                        </div>
                                     </div>
-                                    <div className={ forOwner ? "d-none" : ""}>
-                                        <TransparentButton width="80px" height="30px" buttonColor="blue" padding="0" margin="10px 0">Chat</TransparentButton>
-                                        <TransparentButton width="80px" height="30px" buttonColor="blue" padding="0" margin="10px 0">Follow</TransparentButton>
+
+                                    <div className={forOwner ? "d-none" : ""}>
+                                        <TransparentButton className="ms-2" width="80px" height="30px" buttonColor="blue" padding="0" margin="10px 0">Chat</TransparentButton>
+                                        <TransparentButton className="ms-2" width="80px" height="30px" buttonColor="blue" padding="0" margin="10px 0">Follow</TransparentButton>
                                     </div>
                                 </div>
-                                <div className={ forOwner ? "d-none" : "d-flex flew-wrap justify-content-around mt-3"}>
+                                <div className={forOwner ? "d-none" : "d-flex flew-wrap justify-content-end mt-3"}>
                                     {/* <SolidButton className={checkFavorite? "d-none" : ""} onClick={() => addFavourite(data._id)} width="132px" fontSize="24px" buttonColor="red" padding="5px" margin="0">
                                         Add to Favorite
                                     </SolidButton>
                                     <SolidButton className={checkFavorite? "" : "d-none"} onClick={() => deleteFavourite(data._id , checkpath)} width="132px" fontSize="24px" buttonColor="orange" padding="5px" margin="0">
                                         Reomve to Favorite
                                     </SolidButton> */}
-                                    <div onClick={handleClickFavorite}>{favorite_btn()}</div>
-                                    <SolidButton onClick={clickRequest} width="132px" fontSize="24px" buttonColor="limegreen" padding="5px" margin="0">
-                                        Request Trading
+                                    <div className="me-3" onClick={handleClickFavorite}>{favorite_btn()}</div>
+                                    <SolidButton onClick={clickRequest} className="px-3" fontSize="24px" buttonColor="limegreen" padding="5px" margin="0" style={{boxShadow: "0 0 8px rgba(10,10,10,0.1)"}}>
+                                        <CgArrowsExchangeAlt className="me-1" style={{filter: "brightness(10)"}}/>Trade
                                     </SolidButton>
                                 </div>
                             </div>
                         </div>
-    
+
                         <div className="">
                             <div className="d-flex mt-3 border border-secondary rounded-3">
-                                <p className="m-0 rounded-left px-4 fw-bold p-1" style={{ color: "white", backgroundColor: "#64B9F6" }}>Require</p>
+                                <p className="m-0 rounded-left px-4 fw-bold p-1 d-flex justify-content-center align-items-center" style={{ color: "white", backgroundColor: "#64B9F6" }}>Require</p>
                                 <div className="d-flex">
-                                {wantCate}
+                                    {wantCate}
                                 </div>
                             </div>
-                            <div className="mt-3 px-3 pb-4 border border-secondary rounded-3" style={{minHeight:"250px"}}>
+                            <div className="mt-3 px-3 pb-4 border border-secondary rounded-3" style={{ minHeight: "250px" }}>
                                 <p className="mb-1 fw-bold fs-3" style={{ color: "black" }}>Details</p>
                                 <p className="m-0">{requireDetail}</p>
                                 <p className={tmpRequireDetailShow}>{tmpRequireDetail}</p>
@@ -249,13 +255,13 @@ function Product() {
                 <Footer />
             </div>
         );
-    }else{
-        return(
+    } else {
+        return (
             <div>
                 <h4>กำลังโหลดข้อมูล</h4>
             </div>
         );
-    }    
+    }
 }
 
 export default Product;
