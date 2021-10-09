9397da1fdebebd8146bc489a12032cc2405d3b1d
FranKydeSU <franapasin.s@gmail.com>
2021-10-10 04:46:59 +0700

    built UI for chat

--- src/components/Chats/Chat.tsx
+++ src/components/Chats/Chat.tsx
@@ -84,7 +84,7 @@ export default function Chat() {
             ) : (
                 <div className="chatContainer">
                     <div className="chatContainer-Header d-flex align-items-center px-5">
-                        <img src="https://data.whicdn.com/images/349884984/original.jpg" className="rounded-circle m-0" style={{ maxWidth: 47 }} />
+                        <img src="https://data.whicdn.com/images/349884984/original.jpg" className="rounded-circle m-0" style={{ maxWidth: "47px" }} />
                         <span className="ms-3 text-white">FranKydeSU</span>
                     </div>
                     <div className="messages" style={{ height: "500px", overflow: "auto" }}>
@@ -93,8 +93,24 @@ export default function Chat() {
                                 <div className="messageContainer"
                                 // id={val.author == userName ? "You" : "Other"}
                                 >
-                                    <div className={val.author == userName ? "yourMsg text-right" : "otherMsg"}>
-                                        {val.message} ({val.author})
+                                    <div className={val.author === userName ? "yourMsg" : "otherMsg"}>
+                                        {val.author === userName ? "" :
+                                            <div style={{ 
+                                                    minWidth: "47px", 
+                                                    minHeight: "47px",
+                                                    maxWidth: "47px", 
+                                                    maxHeight: "47px",
+                                                    backgroundImage:"url(https://lordsofgaming.net/wp-content/uploads/2020/10/Screen-Shot-2020-10-19-at-11.15.33-PM.png)",
+                                                    backgroundSize: 'cover' , 
+                                                    backgroundRepeat: 'no-repeat',
+                                                    borderRadius:"50%",
+                                                    backgroundPosition:"center"
+                                                }}>
+                                            </div>
+                                        }
+                                        <div className={val.author === userName ? "owntextBox" : "othertextBox"}>
+                                            {val.message} ({val.author})
+                                        </div>
                                     </div>
                                 </div>
                             );
